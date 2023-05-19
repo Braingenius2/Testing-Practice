573175559c8239e1cdea56282595e93e3de276d9
Braingenius2 <braingenius2@gmail.com>
2023-05-19 18:41:47 +0300

    Expand stringLength()  to throw error. Add test

--- stringLength.js
+++ stringLength.js
@@ -1,3 +1,6 @@
 export function stringLength(string) {
+  if (string.length < 1 || string.length > 10) {
+    throw new Error('String must be between 1 and 10 characters long');
+  }
   return string.length;
 }
